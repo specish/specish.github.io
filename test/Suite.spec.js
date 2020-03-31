9910ba1c1a5efadda746a899444a359e3b78e594
Ken Whatmough <ken.whatmough@bluescape.com>
2020-03-31 10:08:45 -0400

    fix rename, more tests

--- test/Suite.spec.js
+++ test/Suite.spec.js
@@ -34,19 +34,27 @@ describe("Suite", () => {
     let mockHandler;
     let mockSpec;
     let mockInnerSuite;
+    let mockPreSpec;
+    let mockPostSpec;
+    let rootSuite;
 
     beforeEach(() => {
       mockHandler = {
         suiteStart: Mock.fn().mockName("suiteStart"),
         suiteEnd: Mock.fn().mockName("suiteEnd")
       };
+
       mockSpec = { run: Mock.fn().mockName("run") };
       mockInnerSuite = { run: Mock.fn().mockName("run") };
+      mockPreSpec = Mock.fn().mockName("mockPreSpec");
+      mockPostSpec = Mock.fn().mockName("mockPostSpec");
 
-      const suite = new Suite(null, description);
-      suite.specs.push(mockSpec);
-      suite.innerSuites.push(mockInnerSuite);
-      suite.run(mockHandler);
+      rootSuite = new Suite(null, description);
+      rootSuite.specs.push(mockSpec);
+      rootSuite.innerSuites.push(mockInnerSuite);
+      rootSuite.preSpecs.push(mockPreSpec);
+      rootSuite.postSpecs.push(mockPostSpec);
+      rootSuite.run(mockHandler);
     });
 
     it("should invoke suiteStart once with description", () => {
@@ -67,6 +75,39 @@ describe("Suite", () => {
     it("should run the inner suite once with handler", () => {
       expect(mockInnerSuite.run).toHaveBeenCalledTimes(1);
       expect(mockInnerSuite.run).toHaveBeenCalledWithShallow(mockHandler);
+    });
+
+    it("should run the pre-spec once with no arguments", () => {
+      expect(mockPreSpec).toHaveBeenCalledTimes(1);
+      expect(mockPreSpec).toHaveBeenCalledWithShallow();
+    });
+
+    it("should run the post-spec once with no arguments", () => {
+      expect(mockPostSpec).toHaveBeenCalledTimes(1);
+      expect(mockPostSpec).toHaveBeenCalledWithShallow();
+    });
+
+    describe("for an inner suite", () => {
+      let innerSuite;
+
+      beforeEach(() => {
+        mockPreSpec.mockClear();
+        mockPostSpec.mockClear();
+
+        innerSuite = new Suite(rootSuite);
+        innerSuite.specs.push({ run: () => {} });
+        innerSuite.run({ suiteStart: () => {}, suiteEnd: () => {} });
+      });
+
+      it("should run the pre-spec of the parent", () => {
+        expect(mockPreSpec).toHaveBeenCalledTimes(1);
+        expect(mockPreSpec).toHaveBeenCalledWithShallow();
+      });
+
+      it("should run the post-spec of the parent", () => {
+        expect(mockPostSpec).toHaveBeenCalledTimes(1);
+        expect(mockPostSpec).toHaveBeenCalledWithShallow();
+      });
     });
   });
 });
